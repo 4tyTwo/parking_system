c8738401e77f8d2e24f70178f3d2e4685f6494d7
Igor Toporkov <i.toporkov@rbkmoney.com>
2019-05-04 05:54:43 +0300

    Fixed json load and multiple responces, minor styling

--- parkingServer.py
+++ parkingServer.py
@@ -7,6 +7,7 @@ import json
 class MyServer(BaseHTTPRequestHandler):
     used_keys = []
     allowed_commands = ['take', 'store']
+    can_continue = True
     def _set_headers(self, code, headers = {}):
         self.send_response(code)
         for header, value in headers.items():
@@ -14,52 +15,67 @@ class MyServer(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
-        print("GET")
+        print('GET')
         self._set_headers(405)
 
     def do_HEAD(self):
-        print("HEAD")
+        print('HEAD')
         self._set_headers(405)
         
     def do_POST(self):
         self.__validate_headers()
+        if not self.can_continue:
+            return
         self.content = self.get_content()
+        if not self.can_continue:
+            return
         print('POST:', self.content)
+        if not self.can_continue:
+            return
         self.__validate_command()
+        if not self.can_continue:
+            return
         self.process_command()
     
     def get_content(self):
         content_length = int(self.headers['Content-Length'])
         try:
-            content = self.rfile.read(content_length)
+            content = json.loads(self.rfile.read(content_length))
         except:
             self.send_error(400, 'Incorrect JSON')
         return content
 
     def __validate_command(self):
         command = self.content
-        if "action" in command:
+        if 'action' in command:
             if command['action'] == 'take':
                 if not 'position' in command:
                     self.send_error(400, 'No position specified')
-                elif command["position"] >= 30 or command["position"] < 0:  # TODO get value from parking lot
-                        self.send_error(400, "Incorrect postion")
+                    self.can_continue = False
+                elif command['position'] >= 30 or command['position'] < 0:  # TODO get value from parking lot
+                        self.send_error(400, 'Incorrect position')
+                        self.can_continue =  False
         else:
             self.send_error(400, 'Missing action')
+            self.can_continue = False
 
     def process_command(self):
         self.send_response(202)
+        self.end_headers()
 
     def __validate_headers(self):
-        map(self.__validate_header_existance, [
-            'Content-type',
-            'Idempotency-Key',
-            'Content-Length'
-        ])
+        for header in ['Content-type', 'Content-Length', 'Idempotency-Key']:
+            if not self.can_continue:
+                return
+            self.__validate_header_existance(header)
+        if not self.can_continue:
+                return
         self.__validate_header_value(
             'Content-type',
             lambda header: header == 'application/json'
         )
+        if not self.can_continue:
+            return
         self.__validate_header_value(
             'Idempotency-Key',
             lambda header: self.__validate_idempotency_key(header)
@@ -67,12 +83,14 @@ class MyServer(BaseHTTPRequestHandler):
     
     def __validate_header_existance(self, headerName):
         if not headerName in self.headers:
-            self.send_error(400, "Missing" + headerName + "header")
+            self.send_error(400, 'Missing ' + headerName + ' header')
+            self.can_continue = False
 
     def __validate_header_value(self, headerName, fun):
         if fun(self.headers[headerName]) == False:
-            self.send_error(400, "Incorrect value of " +
-                            headerName + " header")
+            self.send_error(400, 'Incorrect value of ' +
+                            headerName + ' header')
+            self.can_continue = False
 
     def __validate_idempotency_key(self, key):
         return key not in self.used_keys and self.__is_valid_uuid(key)
@@ -98,11 +116,11 @@ class MyServer(BaseHTTPRequestHandler):
         return str(uuid_obj) == uuid_to_test
 
 
-hostName = ""
+hostName = ''
 hostPort = 4242
 
 myServer = HTTPServer((hostName, hostPort), MyServer)
-print("Server Starts - %s:%s" % (hostName, hostPort))
+print('Server Starts - %s:%s' % (hostName, hostPort))
 
 try:
 	myServer.serve_forever()
